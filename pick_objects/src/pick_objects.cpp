5fda46a27b916097a9cdcac3b91afab03ce8feb5
Matt Cotton <mattcotton20@outlook.com>
2020-08-25 06:34:43 +0000

    Update pick_objects to communicate with add_markers

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -1,6 +1,29 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <std_msgs/String.h>
+
+// Define a publisher to send object status messages to the add_markers node
+class PickObjectsPublisher
+{
+public:
+  PickObjectsPublisher()
+  {
+    // Create topic to send object status messages
+    pub_status = n_.advertise<std_msgs::String>("/object_status", 1);
+
+  }
+
+  // Public method to publish the object status
+  void publish(std_msgs::String status_msg){
+    pub_status.publish(status_msg);
+  }
+
+private:
+  ros::NodeHandle n_; 
+  ros::Publisher pub_status;
+
+};
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -12,16 +35,28 @@ int main(int argc, char** argv){
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
+  //Instance of publisher class
+  PickObjectsPublisher status_publisher;
+
+  //Variables to hold status message
+  std_msgs::String msg;
+  std::string str_status;
+
   // Wait 5 sec for move_base action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  // Target positions
-  float pickUpPose[3] = {0.1, 3.0, 3.1415};
-  float dropOffPose[3] = {-0.5, 0.5, 0.0};
+  // Publish initial status
+  str_status = "pickup";
+  msg.data = str_status;
+  status_publisher.publish(msg);
 
-  // ---------- PICK UP ----------
+  // Target positions
+  float pickUpPose[3] = {7.0, 0.5, 1.5707};
+  float dropOffPose[3] = {5.0, -3.5, 1.5707};
+
+  //Define pick up goal
 
   move_base_msgs::MoveBaseGoal pickUpGoal;
 
@@ -34,23 +69,7 @@ int main(int argc, char** argv){
   pickUpGoal.target_pose.pose.position.y = pickUpPose[1];
   pickUpGoal.target_pose.pose.orientation.w = pickUpPose[2];
 
-   // Send the goal position and orientation for the robot to reach
-  ac.sendGoal(pickUpGoal);
-  ROS_INFO("Robot is travelling to the pickup zone");
-
-  // Wait an infinite time for the results
-  ac.waitForResult();
-
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Robot picked up the virtual object");
-    ros::Duration(5.0).sleep(); 
-  }
-  else
-    ROS_INFO("Robot failed to travel to the pickup zone");
-
-
-  // ---------- DROP OFF ----------
+  // Define drop off goal
 
   move_base_msgs::MoveBaseGoal dropOffGoal;
 
@@ -64,20 +83,48 @@ int main(int argc, char** argv){
   dropOffGoal.target_pose.pose.position.y = dropOffPose[1];
   dropOffGoal.target_pose.pose.orientation.w = dropOffPose[2];
 
-   // Send the goal position and orientation for the robot to reach
-  ac.sendGoal(dropOffGoal);
-  ROS_INFO("Robot is travelling to the drop off zone");
+
+  // Send pick up goal
+  ac.sendGoal(pickUpGoal);
+  ROS_INFO("Robot is travelling to the pickup zone");
 
   // Wait an infinite time for the results
   ac.waitForResult();
 
-  // Check if the robot reached its goal
+  // Check if the robot reached the pick up goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Robot dropped off the virtual object");
+    ROS_INFO("Robot picked up the virtual object");
+
+    //Publish updated status
+    str_status = "held";
+    msg.data = str_status;
+    status_publisher.publish(msg);
+
     ros::Duration(5.0).sleep(); 
+
+    // Send the drop off goal
+    ac.sendGoal(dropOffGoal);
+    ROS_INFO("Robot is travelling to the drop off zone");
+
+    // Wait an infinite time for the results
+    ac.waitForResult();
+
+    // Check if the robot reached its goal
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+      ROS_INFO("Robot dropped off the virtual object");
+
+      //Publish updated status
+      str_status = "dropoff";
+      msg.data = str_status;
+      status_publisher.publish(msg);
+
+      ros::Duration(5.0).sleep(); 
+    }
+    else
+      ROS_INFO("Robot failed to travel to the drop off zone");
   }
   else
-    ROS_INFO("Robot failed to travel to the drop off zone");
-
-  return 0;
+    ROS_INFO("Robot failed to travel to the pickup zone");
 }
+
+
