f9d4d78b1def931cc2073602a5606bc9d8345a94
Matt Cotton <mattcotton20@outlook.com>
2020-08-25 06:34:10 +0000

    Create add_markers package

---
+++ scripts/add_markers.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Define workspace location
+path_catkin_ws="/home/workspace/catkin_ws"
+
+export TURTLEBOT_GAZEBO_WORLD_FILE=${path_catkin_ws}/src/worlds/matt.world
+export TURTLEBOT_GAZEBO_MAP_FILE=${path_catkin_ws}/src/map/map.yaml
+export ROBOT_INITIAL_POSE="-x 0 -y 0 -Y 1.5707"
+
+# Run turtlebot_world.launch
+xterm  -e  "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+
+# Run amcl_demo.launch
+xterm  -e  "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" & 
+sleep 5
+
+# Run view_navigation.launch
+xterm  -e  "cd ${path_catkin_ws}; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+# Run add_markers node
+xterm  -e  "cd ${path_catkin_ws}; source devel/setup.bash; rosrun add_markers add_markers"
