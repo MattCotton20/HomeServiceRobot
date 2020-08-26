6181ecb7f5aada6d6cb36325d2a42e295b4b8061
Matt Cotton <mattcotton20@outlook.com>
2020-08-26 08:59:19 +0000

    Rewrite nodes (see below)
    * Rewrite .cpp nodes from initial templates provided by Udacity, without using other students' examples utilising class structures as reference
    * Modify /object_status topic to use Char messages, so switch-case statements can be used in add_markers.cpp
    * Simplify MoveBaseGoal definitions in pick_objects.cpp to reduce code repetition

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,116 +1,107 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
-#include <std_msgs/String.h>
+#include <std_msgs/Char.h>
 
-class AddMarkers {
-public:
+// Define global variables
+visualization_msgs::Marker marker;
+ros::Publisher marker_pub;
 
-  visualization_msgs::Marker marker;
+void object_status_callback(const std_msgs::Char status)
+{
+  switch (status.data) {
 
-  AddMarkers()
-  {
-    //Publisher
-    pub_ = n_.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  // Pick up
+  case 'p':
+    // Display marker at pick up position
+    marker.pose.position.x = 7.0;
+    marker.pose.position.y = 0.5;
+    marker.pose.position.z = 0.15;
 
-    //Subscribe to /object_status from the pick_objects node to know where to display marker
-    sub_status = n_.subscribe("/object_status", 1, &AddMarkers::marker_status_callback, this);
-	   
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-
-    // Set the namespace and id for this marker
-    marker.ns = "add_markers";
-    marker.id = 0;
-
-    // Set the marker type to sphere
-    marker.type = visualization_msgs::Marker::SPHERE;
-
-    // Add the marker with default orientation
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-
-    // Set the scale of the marker
-    marker.scale.x = 0.3;
-    marker.scale.y = 0.3;
-    marker.scale.z = 0.3;
-
-    // Set the color to green
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
+    // Make marker visible
     marker.color.a = 1.0;
 
-    marker.lifetime = ros::Duration();
+    ROS_INFO("Marker displayed at pickup zone");
+    break;
+
+  // Held
+  case 'h':
+    // Make marker invisible
+    marker.color.a = 0.0;
+
+    ROS_INFO("Marker hidden");
+    break;
+
+  // Drop off
+  case 'd':
+    // Display marker at drop off position
+    marker.pose.position.x = 5.0;
+    marker.pose.position.y = -3.5;
+    marker.pose.position.z = 0.15;
+
+    // Make marker visible
+    marker.color.a = 1.0;
+
+    ROS_INFO("Marker displayed at drop off zone");
+    break;
+
+  default:
+    ROS_WARN("Invalid object_status message");
   }
+  marker_pub.publish(marker);
+}
 
-  //Function to publish marker
-  void PublishMarker()
-  {
-    pub_.publish(marker);
-  }
-
-  //Callback function for the /object_status topic
-  void marker_status_callback(const std_msgs::String msg)
-  {
-    if (msg.data == "pickup")
-    {
-      // Display marker at pick up position
-      this->SetMarkerPosition(7.0, 0.5, 0.15);
-      this->SetVisibility(1.0);
-      ROS_INFO("Marker displayed at pickup zone");
-    }
-    else if (msg.data == "held")
-    {
-      // Hide marker to indicate being held
-      this->SetVisibility(0.0);
-      ROS_INFO("Marker hidden");
-    }
-    else
-    {
-      // Display marker at drop off position
-      this->SetMarkerPosition(5.0, -3.5, 0.15);
-      this->SetVisibility(1.0);
-      ROS_INFO("Marker displayed at drop off zone");
-    }
-    pub_.publish(marker);
-  }
-
-
-private:
-  //Private variables
-  ros::NodeHandle n_; 
-  ros::Publisher pub_;
-  ros::Subscriber sub_status;
-
-  //Function to change visibility of marker
-  void SetVisibility(double a){
-     marker.color.a = a;
-  }
-
-  //Function to change position of marker
-  void SetMarkerPosition (double x,double y, double z){
-
-    // Set the pose of the marker
-    marker.pose.position.x = x;
-    marker.pose.position.y = y;
-    marker.pose.position.z = z;
-  }
- 
-};
 
 int main( int argc, char** argv )
 {
-  // Initialise node
+  // Initialise the add_markers node and create a handle to it
   ros::init(argc, argv, "add_markers");
+  ros::NodeHandle n;
 
-  //Call AddMarkers constructor
-  AddMarkers newAddMarkers;
+  // Define marker publisher to send marker data to RViz 
+  marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+  // Define object_status publisher to listen for when the virtual object is moved
+  ros::Subscriber object_status_sub = n.subscribe("/object_status", 1, object_status_callback);
+
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker.header.frame_id = "map";
+  marker.header.stamp = ros::Time::now();
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker.ns = "virtual_object";
+  marker.id = 0;
+
+  // Set the market shape type to be a sphere
+  marker.type = visualization_msgs::Marker::SPHERE;
+
+  // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+  marker.action = visualization_msgs::Marker::ADD;
+
+  // Set default pose of the marker
+  marker.pose.position.x = 0;
+  marker.pose.position.y = 0;
+  marker.pose.position.z = 0;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker.scale.x = 0.3;
+  marker.scale.y = 0.3;
+  marker.scale.z = 0.3;
+
+  // Set the color to green, and invisible so it is not initially shown
+  marker.color.r = 0.0f;
+  marker.color.g = 1.0f;
+  marker.color.b = 0.0f;
+  marker.color.a = 0.0;
+
+  marker.lifetime = ros::Duration();
 
   ros::spin();
 
   return 0;
-
 }
+
--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -1,29 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-#include <std_msgs/String.h>
-
-// Define a publisher to send object status messages to the add_markers node
-class PickObjectsPublisher
-{
-public:
-  PickObjectsPublisher()
-  {
-    // Create topic to send object status messages
-    pub_status = n_.advertise<std_msgs::String>("/object_status", 1);
-
-  }
-
-  // Public method to publish the object status
-  void publish(std_msgs::String status_msg){
-    pub_status.publish(status_msg);
-  }
-
-private:
-  ros::NodeHandle n_; 
-  ros::Publisher pub_status;
-
-};
+#include <std_msgs/Char.h>
 
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -32,99 +10,89 @@ int main(int argc, char** argv){
   // Initialize the pick_objects node
   ros::init(argc, argv, "pick_objects");
 
-  //tell the action client that we want to spin a thread by default
+  // Create a handle to the pick_objects node
+  ros::NodeHandle n;
+
+  // Tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
 
-  //Instance of publisher class
-  PickObjectsPublisher status_publisher;
+  // Create a publisher that can tell the add_markers node the status of the virtual object
+  ros::Publisher object_status_pub = n.advertise<std_msgs::Char>("/object_status", 1);
 
-  //Variables to hold status message
-  std_msgs::String msg;
-  std::string str_status;
-
-  // Wait 5 sec for move_base action server to come up
+  // Wait for the move_base action server to start
   while(!ac.waitForServer(ros::Duration(5.0))){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
-  // Publish initial status
-  str_status = "pickup";
-  msg.data = str_status;
-  status_publisher.publish(msg);
+  // object_status message variable
+  std_msgs::Char status;
+
+  // Publish initial status (at pick up zone)
+  status.data = 'p';
+  object_status_pub.publish(status);
 
   // Target positions
   float pickUpPose[3] = {7.0, 0.5, 1.5707};
   float dropOffPose[3] = {5.0, -3.5, 1.5707};
 
-  //Define pick up goal
+  // Create goal object
+  move_base_msgs::MoveBaseGoal goal;
 
-  move_base_msgs::MoveBaseGoal pickUpGoal;
+  // Set up the frame parameters
+  goal.target_pose.header.frame_id = "map";
+  goal.target_pose.header.stamp = ros::Time::now();
 
-  // set up the frame parameters
-  pickUpGoal.target_pose.header.frame_id = "map";
-  pickUpGoal.target_pose.header.stamp = ros::Time::now();
-
-  // Define a position and orientation for the robot to reach
-  pickUpGoal.target_pose.pose.position.x = pickUpPose[0];
-  pickUpGoal.target_pose.pose.position.y = pickUpPose[1];
-  pickUpGoal.target_pose.pose.orientation.w = pickUpPose[2];
-
-  // Define drop off goal
-
-  move_base_msgs::MoveBaseGoal dropOffGoal;
-
-
-  // set up the frame parameters
-  dropOffGoal.target_pose.header.frame_id = "map";
-  dropOffGoal.target_pose.header.stamp = ros::Time::now();
-
-  // Define a position and orientation for the robot to reach
-  dropOffGoal.target_pose.pose.position.x = dropOffPose[0];
-  dropOffGoal.target_pose.pose.position.y = dropOffPose[1];
-  dropOffGoal.target_pose.pose.orientation.w = dropOffPose[2];
-
+  // Set target location as pick up zone
+  goal.target_pose.pose.position.x = pickUpPose[0];
+  goal.target_pose.pose.position.y = pickUpPose[1];
+  goal.target_pose.pose.orientation.w = pickUpPose[2];
 
   // Send pick up goal
-  ac.sendGoal(pickUpGoal);
-  ROS_INFO("Robot is travelling to the pickup zone");
+  ac.sendGoal(goal);
+  ROS_INFO("Travelling to the pickup zone");
 
   // Wait an infinite time for the results
   ac.waitForResult();
 
   // Check if the robot reached the pick up goal
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Robot picked up the virtual object");
+    ROS_INFO("Picked up the virtual object");
 
-    //Publish updated status
-    str_status = "held";
-    msg.data = str_status;
-    status_publisher.publish(msg);
+    // Publish updated status (virtual object is being held)
+    status.data = 'h';
+    object_status_pub.publish(status);
 
-    ros::Duration(5.0).sleep(); 
+    // Pause 5 seconds to imitate picking up object
+    ros::Duration(5.0).sleep();  
 
-    // Send the drop off goal
-    ac.sendGoal(dropOffGoal);
-    ROS_INFO("Robot is travelling to the drop off zone");
+    // Update target location to be drop off zone
+    goal.target_pose.pose.position.x = dropOffPose[0];
+    goal.target_pose.pose.position.y = dropOffPose[1];
+    goal.target_pose.pose.orientation.w = dropOffPose[2];
+
+    // Send drop off goal
+    ac.sendGoal(goal);
+    ROS_INFO("Travelling to the drop off zone");
 
     // Wait an infinite time for the results
     ac.waitForResult();
 
     // Check if the robot reached its goal
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-      ROS_INFO("Robot dropped off the virtual object");
+      ROS_INFO("Dropped off the virtual object");
 
-      //Publish updated status
-      str_status = "dropoff";
-      msg.data = str_status;
-      status_publisher.publish(msg);
+      // Publish updated status (at drop off zone)
+      status.data = 'd';
+      object_status_pub.publish(status);
 
+      // Pause 5 seconds to imitate dropping object
       ros::Duration(5.0).sleep(); 
     }
     else
-      ROS_INFO("Robot failed to travel to the drop off zone");
+      ROS_INFO("Failed to travel to the drop off zone");
   }
   else
-    ROS_INFO("Robot failed to travel to the pickup zone");
+    ROS_INFO("Failed to travel to the pick up zone");
 }
 
 
